7fca22494001f9fdd6fe4fa5745afaba984fc035
Ryutaro <ricken0203@gmail.com>
2017-01-12 01:07:03 +0900

    initial commit

---
+++ binarysearch.rb
@@ -0,0 +1,18 @@
+def binary_search(arr_size, value)
+  arr = (1..arr_size).to_a
+
+  left = 0
+  right = arr.last
+  mid = 0
+
+  while left <= right do
+    mid = (left + right) / 2
+    if arr[mid] == value
+      return "Found!"
+    elsif arr[mid] < value
+      left = mid + 1
+    else
+      right = mid - 1
+    end
+  end
+end
