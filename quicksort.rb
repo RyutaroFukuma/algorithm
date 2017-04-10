e006e60f8e45c11428022f3f828fe9ea66164b8f
Ryutaro <ricken0203@gmail.com>
2017-04-11 02:08:53 +0900

    brush-up

--- quicksort.rb
+++ quicksort.rb
@@ -7,7 +7,7 @@ def average(ary)
 end
 
 def average_sample(ary, count)
-  average(count.times.map{ary.sample})
+  average(count.times.map { ary.sample })
 end
 
 # 定数は適当
@@ -25,13 +25,13 @@ def qsort ary
     return ary
   elsif ary.length == 2
     return ary if ary[0] < ary[1]
-    return [ary[1],ary[0]]
+    return [ary[1], ary[0]]
   end
 
   pivot = select_pivot ary, ary.length
   left = []
   right = []
-  ary.each{|num|
+  ary.each { |num|
     if num < pivot
       left << num
     else
@@ -43,7 +43,7 @@ end
 
 require 'pp'
 
-array = 10.times.map{ rand(100)} # [0..100)の整数10個の配列を生成
+array = 10.times.map { rand(100) } # [0..100)の整数10個の配列を生成
 pp array
 pp (sorted_array = qsort(array))
 puts "is_sorted: #{array.sort == sorted_array}"
